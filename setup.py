17d3ac71c68cf40da76c67f87b638e7b97f1c599
Proc3ss <Proc3sselevated@gmail.com>
2014-04-10 19:46:30 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
+'''
+
+Currently using PyInstall not this setup.py
+
+'''
+
+
 import py2exe
 from distutils.core import setup
 import sys, os
@@ -17,5 +24,4 @@ setup(
     description='Eve Online Combat Log Analyzer',
     packages=['reVEmind', 'reVEmind.ui', 'reVEmind.classes'],
     data_files = [('sqldrivers', ('C:\Python27\Lib\site-packages\PyQt4\plugins\sqldrivers\qsqlite4.dll',))],
-    options={'py2exe': {'includes': ['scipy.sparse.csgraph._validation', 'os',  'sip', 'PyQt4.QtGui', 'PyQt4.QtCore', 'PyQt4.QtSql', 're', 'sqlite3', 'sys', 'locale', 'zmq.utils', 'zmq.utils.jsonapi', 'zmq.utils.strtypes']}}
 )
